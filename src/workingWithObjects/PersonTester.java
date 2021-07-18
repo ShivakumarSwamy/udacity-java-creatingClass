e9234d375696537209fa2d07316589ef07aca0a8
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2021-07-18 18:47:16 +0200

    lesson 3: working with objects
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

---
+++ src/workingWithObjects/PersonTester.java
@@ -0,0 +1,13 @@
+package workingWithObjects;
+
+public class PersonTester {
+
+    public static void main(String[] args) {
+
+        var bobPerson = new Person("Bob", "Phillips");
+        var mikePerson = new Person("Mike", "Lipson");
+
+        System.out.println(bobPerson);
+        System.out.println(mikePerson);
+    }
+}
